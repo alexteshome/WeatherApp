77658693ac55ad322f9bb4a69610c2b02b976785
alexteshome <alex_teshome@hotmail.com>
2018-12-04 05:39:32 -0500

    Updates

---
+++ precache-manifest.ffcbaa951560b59e6bbe46793ffcf50b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a65dfe9f73dd55aa5b62",
+    "url": "/WeatherApp/static/js/runtime~main.a65dfe9f.js"
+  },
+  {
+    "revision": "5eeb5b182455b3b6b9d8",
+    "url": "/WeatherApp/static/js/main.5eeb5b18.chunk.js"
+  },
+  {
+    "revision": "11890a2d43f33613ecb4",
+    "url": "/WeatherApp/static/js/1.11890a2d.chunk.js"
+  },
+  {
+    "revision": "5eeb5b182455b3b6b9d8",
+    "url": "/WeatherApp/static/css/main.9d78ac97.chunk.css"
+  },
+  {
+    "revision": "11890a2d43f33613ecb4",
+    "url": "/WeatherApp/static/css/1.3d36e4dc.chunk.css"
+  },
+  {
+    "revision": "14db69bfb5ea7a06416f7a8644d8ba4b",
+    "url": "/WeatherApp/index.html"
+  }
+];
